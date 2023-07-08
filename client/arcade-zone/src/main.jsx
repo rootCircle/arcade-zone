7ae685d0b43e517fbc578f32b4e8fbe0eae81c0b
Lab Rat <dev.frolics@gmail.com>
2023-07-08 14:13:30 +0530

    Fixed width issue in root div

--- client/arcade-zone/src/main.jsx
+++ client/arcade-zone/src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { ChakraProvider } from '@chakra-ui/react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import './index.css'
 
 const rootElement = document.getElementById('root')
 ReactDOM.createRoot(rootElement).render(
