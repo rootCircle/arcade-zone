adbf666155e2084ceebbcea5f0628cf35f2425cc
DevChandna <devchandna15@gmail.com>
2023-07-08 19:58:11 +0530

    Resolved merge conflict

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
-import { useState } from 'react'
-import ContactUs from './pages/ContactUs'
+import ContactUs from './pages/ContactUs/ContactUs'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+import Login from './pages/Login/Login'
+import Error404 from './pages/Error404/Error404'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React from 'react'
 import HomePage from './pages/Homepage/HomePage'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      {/* <ContactUs/> */}
-      <HomePage />
+      <Navbar/>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage/>}/>
+          <Route path="/login" element={<Login/>}/>
+          <Route path="/team" element={<ContactUs/>}/>
+          <Route path="*" element={<Error404/>} />
+        </Routes>
+      </BrowserRouter>
+      <Footer/>
     </>
   )
 }
