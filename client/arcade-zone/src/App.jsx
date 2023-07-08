6f170fe13432578da8cb7e5acb7fe744443a9120
Lab Rat <dev.frolics@gmail.com>
2023-07-08 14:12:48 +0530

    Added login pages

--- client/arcade-zone/src/App.jsx
+++ client/arcade-zone/src/App.jsx
@@ -1,11 +1,20 @@
 import ContactUs from './pages/ContactUs/ContactUs'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
+import Login from './pages/Login/Login'
+// import LoginTest from './pages/Login/LoginTest'
+// import AuthLayout from './layouts/Auth'
+// import Navbar2 from './components/Navbars/AuthNavbar'
+import React from 'react'
 function App() {
   return (
     <>
       <Navbar/>
-      <ContactUs/>
+      {/* <ContactUs/> */}
+      <Login/>
+      {/* <AuthLayout /> */}
+      {/* <Navbar2/> */}
+      {/* <LoginTest/> */}
       <Footer/>
     </>
   )
