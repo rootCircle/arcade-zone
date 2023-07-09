10ec929aade41fde47514df3e71b541955c0c62b
Manish <manishrawat3703@gmail.com>
2023-07-09 21:06:14 +0530

    added gaming page

--- client/arcade-zone/src/App.jsx
+++ client/arcade-zone/src/App.jsx
@@ -1,4 +1,4 @@
-import ContactUs from './pages/ContactUs/ContactUs'
+ import ContactUs from './pages/ContactUs/ContactUs'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import Login from './pages/Login/Login'
@@ -6,19 +6,24 @@ import Error404 from './pages/Error404/Error404'
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import React from 'react'
+import GamingPage from './pages/GamingPage/GamingPage'
+import GamingReview from './pages/GamingPage/GamingReview'
 
 function App() {
   return (
     <>
       <Navbar/>
-      <BrowserRouter>
+      {/* <BrowserRouter>
         <Routes>
           <Route path="/" element={<Login/>}/>
           <Route path="/login" element={<Login/>}/>
           <Route path="/team" element={<ContactUs/>}/>
           <Route path="*" element={<Error404/>} />
         </Routes>
-      </BrowserRouter>
+      </BrowserRouter> */}
+      <GamingPage link="https://paper-io.com/?mode=paper2" name="HOT Game"/> 
+      {/* <GamingReview/>
+      <GamingReview/> */}
       <Footer/>
     </>
   )
